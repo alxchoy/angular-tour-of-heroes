8344d4eea7b0aa61a586e744c55adae0f86ae0f5
Choy <alexanderchoy@multiplica.com>
2017-03-06 15:39:19 -0500

    Mostrar heroe y cambiarlo según el valor ingresado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id:number;
+  name:string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `,
 })
+
+// el ngModel me sirve para actualizar data en lugares distintos y relacionados, haciendo que un valor
+// se actualice para todos los que lo están utilizando.
+
 export class AppComponent  {
   title: string = 'Tour of Heroes';
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   }
-}
-
-export class Hero {
-  id:number;
-  name:string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; // con este módulo podemos realizar two-way binding - enlaces bidireccionales
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+                  BrowserModule,
+                  FormsModule // declaramos el módulo
+                ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
